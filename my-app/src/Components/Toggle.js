cfb3482d1b2d8db25799e4f9a57567b1367f4e93
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-17 11:44:25 +0200

    Created Toggle.js

---
+++ my-app/src/Components/Toggle.js
@@ -0,0 +1,18 @@
+// toggle.js
+
+import React, { useState } from "react";
+
+export default function Toggle(props) {
+  const [state, setState] = useState(false);
+  return (
+    <button
+      onClick={() => {
+        setState(previousState => !previousState);
+        props.onChange(!state);
+      }}
+      data-testid="toggle"
+    >
+      {state === true ? "Turn off" : "Turn on"}
+    </button>
+  );
+}
